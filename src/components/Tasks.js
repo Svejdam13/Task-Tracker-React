1677c77dc4173128b582752ffdba8900281dc0e7
Martin Svejda <svejdam13@gmail.com>
2021-02-11 09:54:13 +0100

    update

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+  {
+    id: 1,
+    text: 'ukol prvni',
+    day: '20 Unor v 16:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'ukol druhy',
+    day: '22 Unor v 11:20',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'ukol treti',
+    day: '25 Unor v 16:50',
+    reminder: false,
+  },
+]
+
+const Tasks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tasks
